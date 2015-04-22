7c82d400f8853b316157f86c7dd2671256091db0
Aleexandr369 <dumi_alexander@yahoo.com>
2015-04-22 23:07:16 +0300

    Dupa_Creare_Membri

---
+++ MyHomework/MyHomework/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomework
+{
+    class Employee : Person
+    {
+        public DateTime dateOfEmployment;
+        public double salary;
+        public int availableDaysOff;
+    }
+}
